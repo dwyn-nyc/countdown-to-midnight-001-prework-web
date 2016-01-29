652182c7a00ad698ac7a431b16410abd48142606
dwyn-nyc <dwyn.nyc@gmail.com>
2016-01-29 14:37:30 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 0
+  while (number) > counter
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    break if (number) == counter
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+  counter = 0
+  while (number) > counter
+    sleep 1
+    puts "'#{number}' SECOND(S)!"
+      number -= 1
+    break if (number) == counter
+  end
+  return "HAPPY NEW YEAR!"
 end
